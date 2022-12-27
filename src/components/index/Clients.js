7dcb0c69a668cfb05a59d34a7b8a936ca9e3e8bf
Sayyed Faisal Ali <80758388+C0DE-SLAYER@users.noreply.github.com>
2022-12-27 04:54:26 +0000

    testi

--- src/components/index/Clients.js
+++ src/components/index/Clients.js
@@ -1,94 +1,117 @@
 import React from 'react'
 import './Clients.css'
+// import { MDBCarousel, MDBCarouselItem, } from 'mdb-react-ui-kit';
+// import Carousel from 'react-bootstrap/Carousel';
+
 
 function Clients() {
   return (
     <>
-      <div class="slider">
-        <div class="container">
-          <div class="section-title mb-3" data-aos="zoom-out">
-            <p>Our Clients</p>
-            {/* <h2>Who are our clients</h2> */}
-          </div>
-        </div>
-        <div class='row' >
-          <div class="slide-track pb-4">
+    <div id="slideshow">
+  <div class="slide-wrapper">
+    <div class="slide"><h1 class="slide-number">1</h1></div>
+    <div class="slide"><h1 class="slide-number">2</h1></div>
+    <div class="slide"><h1 class="slide-number">3</h1></div>
+    <div class="slide"><h1 class="slide-number">4</h1></div>
+    <div class="slide"><h1 class="slide-number">5</h1></div>
+  </div>
+</div>
 
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__1.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__2.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__3.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__4.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__5.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__6.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__7.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__8.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__9.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__10.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__11.jpg" />
-            </div>
-          </div>
-          <div class="slide-track">
-
-
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__12.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__13.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__14.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__15.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__16.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__17.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__18.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__19.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__20.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__15.jpg" />
-            </div>
-            <div class="slide">
-              <img class="img-scroll" src="/img/clients/Client__21.jpg" />
-            </div>
-          </div>
-
-        </div>
-      </div>
+<h1>Just a simple slideshow. CSS only, no JS</h1>
+    
     </>
+
+
+
+
+
+
+    // <>
+    //   <div class="slider">
+    //     <div class="container">
+    //       <div class="section-title mb-3" data-aos="zoom-out">
+    //         <p>Our Clients</p>
+    //         {/* <h2>Who are our clients</h2> */}
+    //       </div>
+    //     </div>
+    //     <div class='row' >
+    //       <div class="slide-track pb-4">
+
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__1.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__2.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__3.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__4.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__5.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__6.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__7.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__8.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__9.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__10.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__11.jpg" />
+    //         </div>
+    //       </div>
+    //       <div class="slide-track">
+
+
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__12.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__13.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__14.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__15.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__16.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__17.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__18.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__19.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__20.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__15.jpg" />
+    //         </div>
+    //         <div class="slide">
+    //           <img class="img-scroll" src="/img/clients/Client__21.jpg" />
+    //         </div>
+    //       </div>
+
+    //     </div>
+    //   </div>
+    // </>
   )
 }
 
